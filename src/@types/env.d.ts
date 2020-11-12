fec30c1e4fc0ba535974e607aac03a11867f7270
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2020-11-12 13:25:03 -0500

    save

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -2,9 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     CORS_ORIGIN: string;
     SECRET: string;
-    POSTGRES_USER: string;
-    POSTGRES_PASSWORD: string;
-    POSTGRES_DB: string;
     HIVE_ACCOUNT: string;
     ACTIVE_WIF: string;
     AWS_ID: string;

32cc579143b2d3cca831c4931f78df7ed9b4dc8c
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2021-02-11 09:34:12 -0500

    save

---
+++ src/models/admin/resolvers/dashboard.ts
@@ -0,0 +1,82 @@
+import dayjs from "dayjs";
+import { Arg, Int, Query, Resolver } from "type-graphql";
+import { Between, FindOperator, getConnection, LessThanOrEqual } from "typeorm";
+import { Comment } from "./../../comment/entities/Comment";
+import { Meme } from "./../../meme/entities/Meme";
+import { RedditMeme } from "./../../reddit/entities/RedditMeme";
+import { User } from "./../../user/entities/User";
+
+@Resolver()
+export class DashboardResolver {
+  @Query(() => [Int])
+  async countGraph(
+    @Arg("ticks", () => Int) ticks: number,
+    @Arg("timeframe") timeframe: string,
+    @Arg("aggro") aggro: string,
+    @Arg("entityType") entityType: string
+  ): Promise<number[]> {
+    if (ticks <= 0 || 100 < ticks) throw new Error("bad tick number");
+    if (!["day", "week", "month"].includes(timeframe))
+      throw new Error("bad timeframe");
+    if (!["new", "total"].includes(aggro)) throw new Error("bad aggro");
+    if (!["user", "meme", "comment"].includes(entityType))
+      throw new Error("bad entityType");
+    const current = dayjs();
+    let createdAt: (idx: number) => FindOperator<Date>;
+    switch (true) {
+      case aggro === "total":
+        createdAt = (idx: number) =>
+          LessThanOrEqual(
+            current
+              .subtract(idx, timeframe as "day" | "week" | "month")
+              .toDate()
+          );
+        break;
+      case aggro === "new":
+        createdAt = (idx: number) =>
+          Between(
+            current
+              .subtract(idx + 1, timeframe as "day" | "week" | "month")
+              .toDate(),
+            current
+              .subtract(idx, timeframe as "day" | "week" | "month")
+              .toDate()
+          );
+        break;
+      default:
+        break;
+    }
+    let counts: Promise<number>[];
+    switch (true) {
+      case entityType === "user":
+        counts = Array.from(Array(ticks).keys()).map(async (idx) => {
+          return await User.count({ where: { createdAt: createdAt(idx) } });
+        });
+        break;
+      case entityType === "meme":
+        counts = Array.from(Array(ticks).keys()).map(async (idx) => {
+          return await Meme.count({ where: { createdAt: createdAt(idx) } });
+        });
+        break;
+      case entityType === "comment":
+        counts = Array.from(Array(ticks).keys()).map(async (idx) => {
+          return await Comment.count({ where: { createdAt: createdAt(idx) } });
+        });
+        break;
+      default:
+        counts = [];
+        break;
+    }
+    return (await Promise.all(counts)).reverse();
+  }
+
+  @Query(() => String)
+  async redditMaxTimestamp(): Promise<String> {
+    const data = await getConnection()
+      .getRepository(RedditMeme)
+      .createQueryBuilder("redditMeme")
+      .select("MAX(redditMeme.createdAt)")
+      .getRawOne();
+    return data.max;
+  }
+}
--- src/models/user/resolvers/query.ts
+++ src/models/user/resolvers/query.ts
@@ -1,6 +1,4 @@
-import dayjs from "dayjs";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
-import { Between, LessThanOrEqual } from "typeorm";
+import { Arg, Ctx, Query, Resolver } from "type-graphql";
 import { ServerContext } from "../../../ServerContext";
 import { User } from "../entities/User";
 
@@ -9,50 +7,6 @@ export class UserQueryResolver {
   @Query(() => [User])
   async users(): Promise<User[]> {
     return await User.find({ take: 20 });
-  }
-
-  @Query(() => [Int])
-  async numNewUsers(@Arg("timeframe") timeframe: string): Promise<number[]> {
-    if (!["day", "week", "month"].includes(timeframe))
-      throw new Error("bad timeframe");
-    const current = dayjs();
-    const counts = Array(10)
-      .fill(10)
-      .map(async (idx) => {
-        return await User.count({
-          where: {
-            createdAt: Between(
-              current
-                .subtract(idx + 1, timeframe as "day" | "week" | "month")
-                .toDate(),
-              current
-                .subtract(idx, timeframe as "day" | "week" | "month")
-                .toDate()
-            ),
-          },
-        });
-      });
-    return Promise.all(counts);
-  }
-  @Query(() => [Int])
-  async numUsers(@Arg("timeframe") timeframe: string): Promise<number[]> {
-    if (!["day", "week", "month"].includes(timeframe))
-      throw new Error("bad timeframe");
-    const current = dayjs();
-    const counts = Array(10)
-      .fill(10)
-      .map(async (idx) => {
-        return await User.count({
-          where: {
-            createdAt: LessThanOrEqual(
-              current
-                .subtract(idx, timeframe as "day" | "week" | "month")
-                .toDate()
-            ),
-          },
-        });
-      });
-    return Promise.all(counts);
   }
 
   @Query(() => User, { nullable: true })

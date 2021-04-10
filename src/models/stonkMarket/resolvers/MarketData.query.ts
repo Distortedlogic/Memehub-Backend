6679531f10fab6ad6b53966d0aaf5c93165cbb0a
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2021-04-10 18:25:33 -0400

    save

--- src/models/stonkMarket/resolvers/MarketData.query.ts
+++ src/models/stonkMarket/resolvers/MarketData.query.ts
@@ -3,18 +3,17 @@ import { Arg, Int, Query, Resolver } from "type-graphql";
 import { InjectRepository } from "typeorm-typedi-extensions";
 import { MarketData } from "../_types";
 import { MarketRepo } from "./../repos/MarketData";
-import { PaginatedMarketData } from "./../_types";
 
 @Resolver(MarketData)
 export class MarketDataQueryResolver {
   @InjectRepository(MarketRepo)
   private readonly marketRepo: MarketRepo;
 
-  @Query(() => PaginatedMarketData)
+  @Query(() => [MarketData])
   async marketHistory(
     @Arg("name") name: string,
     @Arg("take", () => Int) take: number
-  ): Promise<PaginatedMarketData> {
+  ): Promise<MarketData[]> {
     const createdAt = dayjs()
       .set("h", 0)
       .set("m", 0)
@@ -23,14 +22,11 @@ export class MarketDataQueryResolver {
       .subtract(take - 1, "d");
     const realTake = Math.min(50, take);
     const marketData = await Promise.all(
-      Array.from(Array(take).keys()).map(
+      Array.from(Array(realTake).keys()).map(
         async (idx) =>
           await this.marketRepo.getMarketData(name, createdAt.add(idx, "d"))
       )
     );
-    return {
-      items: marketData,
-      hasMore: marketData.length === realTake ? true : false,
-    };
+    return marketData;
   }
 }
